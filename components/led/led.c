b03041119066af1829dee62fc9e963c66b386ec9
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-06-13 09:42:22 +0800

    led light

--- components/led/led.c
+++ components/led/led.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "led.h"
 
-void func(void)
+void led_init(void)
 {
-    printf("hello xjh \r\n");
+    gpio_config_t gpio_init_struct = {0};
+
+    gpio_init_struct.intr_type = GPIO_INTR_DISABLE;        
+    gpio_init_struct.mode = GPIO_MODE_INPUT_OUTPUT;        
+    gpio_init_struct.pull_up_en = GPIO_PULLUP_ENABLE;     
+    gpio_init_struct.pull_down_en = GPIO_PULLDOWN_DISABLE; 
+    gpio_init_struct.pin_bit_mask = 1ull << LED_GPIO_PIN;  
+    gpio_config(&gpio_init_struct);                        
+
+    LED(1); /* 关闭LED */
 }
--- main/main.c
+++ main/main.c
@@ -5,26 +5,17 @@
 #include "esp_chip_info.h"
 #include "esp_psram.h"
 #include "esp_flash.h"
+#include "led.h"
 
 void app_main(void)
 {
-    esp_err_t ret;
-    uint32_t flash_size;
-    esp_chip_info_t chip_info; 
-    ret = nvs_flash_init();    
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    esp_flash_get_size(NULL, &flash_size); 
-    esp_chip_info(&chip_info);
-    printf("内核：cup数量%d\n", chip_info.cores); 
-    printf("FLASH size:%ld MB flash\n", flash_size / (1024 * 1024));        
-    printf("PSRAM size: %d bytes\n", esp_psram_get_size());
+    led_init();
+    printf("led init\r\n");
     while (1)
     {
-        printf("Hello-ESP32\r\n");
+        LED(1);
+        vTaskDelay(1000);
+        LED(0);
         vTaskDelay(1000);
     }
 }
